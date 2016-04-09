478a5aa0780d9e719946505fa6594e896162e4ec
tnm4s <tnm4s@virginia.edu>
2016-04-09 13:22:45 -0400

    completed number wizard script

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -2,24 +2,58 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
 
 	// Use this for initialization
 	void Start () {
-		print("Welcome to Number Wizard"); // Semi-colons at end of C#
-		print("Pick a number in your head, but don't tell me!");
+		StartGame();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print("I won!");
+			StartGame();
+		}
+	}
 
-		int max = 1000;
-		int min = 1;
+	void StartGame ()
+	{
+		max = 1000;
+		min = 1;
+		guess = 500;
+
+		print("================================");
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head, but don't tell me!");
 
 		print("The highest number you can pick is " + max + ".");
 		print("The lowest number you can pick is " + min + ".");
 
-		print("Is the number high or lower than 500?");
-		print("Up arrow for higher and down arrow for lower.");
+		max = max + 1; // Avoids 999 rounding problem
+
+		MakeGuess();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void MakeGuess ()
+	{
+		print("Up = higher, down = lower, return = equal");
+		print("Is the number high or lower than " + guess + "?");
+	}
+
+	void NextGuess ()
+	{
+		guess = (max + min) / 2;
+		print("Is the number high or lower than " + guess + "?");
+		MakeGuess();
 	}
 }
